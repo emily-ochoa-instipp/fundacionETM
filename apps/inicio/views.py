72eeaff47605c3d0869a919d42e89f041b1a8d45
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2025-11-21 12:41:52 -0500

    act eventos

--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from datetime import date
+from django.utils import timezone
 from apps.eventos.models import Evento
 from apps.proyectos.models import Proyecto
 from apps.usuarios.models import Usuario
@@ -20,12 +21,17 @@ def inicio(request):
     proyectos_activos = Proyecto.objects.filter(estado=True).count()
     usuarios_activos = Usuario.objects.filter(user__is_active=True).count()
     eventos_hoy = Evento.objects.filter(fecha=date.today()).count()
+    eventos_proximos = Evento.objects.filter(fecha__gte=timezone.now()).count()
+    # eventos_proximos = Evento.objects.filter(fecha__gte=date.today()).order_by('fecha')
+ 
+
 
     context = {
         # 'total_voluntarios': total_voluntarios,
         'proyectos_activos': proyectos_activos,
         'usuarios_activos': usuarios_activos,
         'eventos_hoy': eventos_hoy,
+        'eventos_proximos': eventos_proximos,
     }
 
     return render(request, 'inicio/inicio.html', context)
