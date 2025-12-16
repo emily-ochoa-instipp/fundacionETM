3368664729c27d3877a1abfda6e14f61119a1a4c
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2025-12-16 11:03:51 -0500

    act estados de eventos

--- apps/eventos/models.py
+++ apps/eventos/models.py
@@ -3,28 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Evento(models.Model):
-    # TIPOS_EVENTO = [
-    #     ('TALLER', 'Taller'),
-    #     ('CHARLA', 'Charla'),
-    #     ('CAMPAÑA', 'Campaña'),
-    #     ('REUNIÓN', 'Reunión'),
-    #     ('FERIA', 'Feria'),
-    #     ('OTRO', 'Otro'),
-    # ]
-
+    ESTADOS_EVENTO = [
+        ('proximo', 'Próximo'),
+        ('realizado', 'Realizado'),
+        ('cancelado', 'Cancelado'),
+    ]
 
     titulo = models.CharField(max_length=150)
     descripcion = models.TextField()
-    # tipo = models.CharField(max_length=20, choices=TIPOS_EVENTO, default='OTRO')
     fecha = models.DateField()
     hora_inicio = models.TimeField(null=True, blank=True)
     hora_fin = models.TimeField(null=True, blank=True)
     lugar = models.CharField(max_length=150)
     direccion = models.CharField(max_length=150)
     imagen = models.ImageField(upload_to='eventos/imagen_portada', null=True, blank=True)
-    #creado_en = models.DateTimeField(auto_now_add=True)
-    #actualizado_en = models.DateTimeField(auto_now=True)
-    estado = models.BooleanField(default=True)
+    estado =  models.CharField(max_length=15, choices=ESTADOS_EVENTO, default='proximo')
 
     def __str__(self):
         return self.titulo
--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -1,34 +1,45 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from apps.eventos.models import Evento, ImagenEvento
+from datetime import date
 
 # Create your views here.
 
 @login_required
 def tabla_eventos(request):
-    eventos = Evento.objects.all()
+    eventos = Evento.objects.all().order_by('-fecha')
     return render(request, 'eventos/tabla_eventos.html', {
         'eventos': eventos
     })
 
+@login_required
 def registrar_evento(request):
+    error = None
     
     if request.method == 'POST':
         titulo = request.POST.get('txtTitulo')
         descripcion = request.POST.get('txtDescripcion')
-        fecha = request.POST.get('txtFecha')
+        fecha_str = request.POST.get('txtFecha')
         hora_inicio = request.POST.get('txtHoraInicio') 
         hora_fin = request.POST.get('txtHoraFin') 
         lugar = request.POST.get('txtLugar')
         direccion = request.POST.get('txtDireccion')
         imagen = request.FILES.get('txtImagen')
+        estado_form = request.POST.get('estado')
 
-        estado = True  
-
-
-        if not (titulo and descripcion and fecha):
+        if not (titulo and descripcion and fecha_str):
             error = "Todos los campos son obligatorios"
         else:
+            fecha = date.fromisoformat(fecha_str)
+            hoy = date.today()
+
+            if estado_form == 'cancelado':
+                estado_final = 'cancelado'
+            elif estado_form == 'realizado':
+                estado_final = 'realizado'
+            else:
+                estado_final = 'realizado' if fecha < hoy else 'proximo'
+            
             Evento.objects.create(
                 titulo=titulo,
                 descripcion=descripcion,
@@ -38,7 +49,7 @@ def registrar_evento(request):
                 lugar=lugar,
                 direccion=direccion,
                 imagen=imagen,
-                estado=estado
+                estado=estado_final
             )
             return redirect('tabla_eventos')
 
@@ -53,27 +64,44 @@ def registrar_evento(request):
 def editar_evento(request, evento_id):
     evento = Evento.objects.get(id=evento_id)
 
+    error = None 
+
     if request.method == 'POST':
         evento.titulo = request.POST.get('txtTitulo')
         evento.descripcion = request.POST.get('txtDescripcion')
-        evento.fecha = request.POST.get('txtFecha')
+
+        fecha_str = request.POST.get('txtFecha')
+        fecha = date.fromisoformat(fecha_str)
+        evento.fecha = fecha
+        
         evento.hora_inicio = request.POST.get('txtHoraInicio')
         evento.hora_fin = request.POST.get('txtHoraFin') 
         evento.lugar = request.POST.get('txtLugar')
         evento.direccion = request.POST.get('txtDireccion')
-        evento.estado = True if request.POST.get('estado') == 'on' else False
+        estado_form = request.POST.get('estado')
+
+        hoy = date.today()
+
+        if estado_form == 'proximo' and evento.fecha < hoy:
+            error = "No puedes marcar como próximo un evento cuya fecha ya pasó."
+
+        elif estado_form == 'realizado' and evento.fecha > hoy:
+            error = "No puedes marcar como realizado un evento con fecha futura."
+
+        else:
+            evento.estado = estado_form
         
-        #imagen portada
-        if request.FILES.get('txtImagen'):
-            evento.imagen = request.FILES['txtImagen']
-        
-        evento.save()
-        
-        return redirect('tabla_eventos')
+            #imagen portada
+            if request.FILES.get('txtImagen'):
+                evento.imagen = request.FILES['txtImagen']
+            
+            evento.save()
+            
+            return redirect('tabla_eventos')
     
     galeria = evento.galeria.all()
 
-    return render(request, 'eventos/editar_evento.html', {'evento': evento, 'galeria': galeria})
+    return render(request, 'eventos/editar_evento.html', {'evento': evento, 'galeria': galeria, 'error': error,'Evento': Evento})
 
 @login_required
 def eliminar_evento(request, evento_id):
