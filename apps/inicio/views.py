dae7ac4d2da30f03792acdfd5f002a0d80d61c95
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2025-12-18 01:12:10 -0500

    act inicio

--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -1,36 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
 from datetime import date
-from django.utils import timezone
+
 from apps.eventos.models import Evento
 from apps.proyectos.models import Proyecto
 from apps.usuarios.models import Usuario
 
 
-# Create your views here.
-
 @login_required
 def inicio(request):
-    return render(request, 'inicio/inicio.html')
+    hoy = date.today()
 
-@login_required
-def inicio(request):
-    # total_voluntarios = Voluntario.objects.count()
-    proyectos_activos = Proyecto.objects.filter(estado=True).count()
+    # PROYECTOS
+    proyectos_activos = Proyecto.objects.filter(estado='en_curso').count()
+    proyectos_realizados = Proyecto.objects.filter(estado='realizado').count()
+
+    # EVENTOS
+    eventos_realizados = Evento.objects.filter(estado='realizado').count()
+    eventos_proximos = Evento.objects.filter(fecha__gte=hoy, estado='proximo').count()
+
+    # USUARIOS
     usuarios_activos = Usuario.objects.filter(user__is_active=True).count()
-    eventos_hoy = Evento.objects.filter(fecha=date.today()).count()
-    eventos_proximos = Evento.objects.filter(fecha__gte=timezone.now()).count()
-    # eventos_proximos = Evento.objects.filter(fecha__gte=date.today()).order_by('fecha')
- 
-
 
     context = {
-        # 'total_voluntarios': total_voluntarios,
         'proyectos_activos': proyectos_activos,
+        'proyectos_realizados': proyectos_realizados,
         'usuarios_activos': usuarios_activos,
-        'eventos_hoy': eventos_hoy,
+        'eventos_realizados': eventos_realizados,
         'eventos_proximos': eventos_proximos,
     }
 
